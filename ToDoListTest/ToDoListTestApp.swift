5a72a62cb6443c8494162e4ebc595f296bfc183a
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-08-05 11:38:59 +0400

    Initial commit

---
+++ ToDoListTest/ToDoListTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTestApp.swift
+//  ToDoListTest
+//
+//  Created by Anton on 05.08.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
